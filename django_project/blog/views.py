27950e2f16aeb2a150789477f547fca5b87c7744
szymonfraszak <szymonfraszak@gmail.com>
2021-06-21 19:27:42 +0200

    Changed views

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Szymon Fraszak',
+        'title': 'Post number one',
+        'content': 'First post content',
+        'date_posted': 'June 16, 2021'
+    },
+    {
+        'author': 'Cristiano Ronaldo',
+        'title': 'Post number two',
+        'content': 'Second post content',
+        'date_posted': 'June 29, 2021'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Home page</h1>')
+    context = {
+        'posts': posts,
+        'title': 'Home Page'
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
